ef8346d1c2486931c9cc8ae95fae48257e06b05a
Jorge Sandoval <jorgesagt5@gmail.com>
2026-03-20 09:25:43 -0700

    Se creó el archivo glrd.proto y se empezó a configurar el proyecto para el desarrollo del Stub inicial

---
+++ backend/src/main/java/com/pechariwi/gargoyle_lrd/service/RansomwareServerService.java
@@ -0,0 +1,8 @@
+package com.pechariwi.gargoyle_lrd.service;
+
+import com.gargoyle.lrd.grpc.Glrd;
+import org.lognet.springboot.grpc.GRpcService;
+
+@GRpcService
+public class RansomwareServerService  {
+}
